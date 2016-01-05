199383fcba3c39dd306217f947960bc634ee0292
tk7000 <s154219@student.dtu.dk>
2016-01-05 13:56:51 +0100

    Vilde ting

--- Puzzle/src/game/Panel.java
+++ Puzzle/src/game/Panel.java
@@ -13,21 +13,31 @@ private Model model;
 	
 	public Panel(){
 		model = new Model(this);
-		GridLayout gLay = new GridLayout(3,3);
+		GridLayout gLay = new GridLayout(1,1);
 		this.setLayout(gLay);
 		
-		JLabel number1 = new JLabel("1");
-		JLabel number2 = new JLabel("2");
-		JLabel number3 = new JLabel("3");
-		JLabel number4 = new JLabel("4");
-		JLabel number5 = new JLabel("5");
-		JLabel number6 = new JLabel("6");
-		JLabel number7 = new JLabel("7");
-		JLabel number8 = new JLabel("8");
-		JLabel number0 = new JLabel(" ");
 		
-		this.add(number1, "Center");
-		this.add(number2, "Center");
+		JLabel number1 = new JLabel("1");
+		number1.setHorizontalAlignment(JLabel.CENTER);
+		JLabel number2 = new JLabel("2");
+		number2.setHorizontalAlignment(JLabel.CENTER);
+		JLabel number3 = new JLabel("3");
+		number3.setHorizontalAlignment(JLabel.CENTER);
+		JLabel number4 = new JLabel("4");
+		number4.setHorizontalAlignment(JLabel.CENTER);
+		JLabel number5 = new JLabel("5");
+		number5.setHorizontalAlignment(JLabel.CENTER);
+		JLabel number6 = new JLabel("6");
+		number6.setHorizontalAlignment(JLabel.CENTER);
+		JLabel number7 = new JLabel("7");
+		number7.setHorizontalAlignment(JLabel.CENTER);
+		JLabel number8 = new JLabel("8");
+		number8.setHorizontalAlignment(JLabel.CENTER);
+		JLabel number0 = new JLabel(" ");
+		JLabel number10 = new JLabel("32");
+		
+		this.add(number1, "Ceter");
+		this.add(number2, "EAST");
 		this.add(number3, "Center");
 		this.add(number4, "Center");
 		this.add(number5, "Center");
@@ -35,7 +45,7 @@ private Model model;
 		this.add(number7, "Center");
 		this.add(number8, "Center");
 		this.add(number0, "Center");
-		
+		this.add(number10, "Center");
 	}
 	
 }
